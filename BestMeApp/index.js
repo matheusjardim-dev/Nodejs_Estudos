47e82eb14c7df9d43175422f037116c6b03efaaf
matheusjardim-dev <matheusjardim.dev@gmail.com>
2023-06-21 10:04:34 -0300

    BestMe

--- BestMeApp/index.js
+++ BestMeApp/index.js
@@ -1,1 +1,30 @@
-console.log('Rodando via script')
+const questions = [
+  "O que eu aprendi hoje?",
+  "O que eu poderia fazer para melhorar?",
+];
+
+const ask = (index = 0) => {
+  process.stdout.write("\n" + questions[index] + "-> ");
+};
+
+ask();
+
+const answers = [];
+process.stdin.on("data", (data) => {
+  answers.push(data.toString().trim());
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+process.on("exit", () => {
+  console.log(`
+    Bacana Matheus!
+
+    Hoje você aprendeu: ${answers[0]}
+
+    E o que você poderia fazer para melhorar é: ${answers[1]}
+    `);
+});
